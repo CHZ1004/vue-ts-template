42db63a66b0edb8dcadbf996ddfdda86a9ed19f5
xianyu <xianyu1004@126.com>
2023-06-21 16:37:20 +0800

    chore: 图标更换

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <h1 class="text-blue-500 bg-red-400">test</h1>
-    <icon-mdi-moon-waning-crescent class="text-3xl text-red-500" />
-    <icon-mdi-white-balance-sunny />
-    <icon-ic-baseline-arrow-forward-ios />
-    <icon-local-vue />
-  </div>
+  <div></div>
 </template>
 
 <style scoped></style>
